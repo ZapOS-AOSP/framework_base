randomaccessmayur/frameworks_base-4

core: Pixel experience Blacklist For Google Photos for Android 13
* See, in Android 13 pixel_experience_2022_midyear was added, which needs to be blacklisted aswell

Change-Id: Id36d12afeda3cf6b39d01a0dbe7e3e9058659b8e

core/java/android/app/ApplicationPackageManager.java
@@ -811,6 +811,7 @@ public class ApplicationPackageManager extends PackageManager {
         String packageName = ActivityThread.currentPackageName();
         if (packageName != null &&
                 packageName.contains("com.google.android.apps.photos") &&
+                name.contains("PIXEL_2022_MIDYEAR_EXPERIENCE") ||
                 name.contains("PIXEL_2021_EXPERIENCE") ||
                 name.contains("PIXEL_2021_MIDYEAR_EXPERIENCE") ||
                 name.contains("PIXEL_2020_EXPERIENCE") ||

ZapOS-AOSP/framework_base

core/java/android/app/ApplicationPackageManager.java
@@ -818,7 +818,8 @@ public class ApplicationPackageManager extends PackageManager {
         String packageName = ActivityThread.currentPackageName();
         if (packageName != null &&
                 packageName.contains("com.google.android.apps.photos") &&
-                (name.contains("PIXEL_2021_EXPERIENCE") ||
+                (name.contains("PIXEL_2022_MIDYEAR_EXPERIENCE") ||
+                name.contains("PIXEL_2021_EXPERIENCE") ||
                 name.contains("PIXEL_2021_MIDYEAR_EXPERIENCE") ||
                 name.contains("PIXEL_2020_EXPERIENCE") ||
                 name.contains("PIXEL_2020_MIDYEAR_EXPERIENCE") ||